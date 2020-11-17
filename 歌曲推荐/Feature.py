6e68fcb93cc5c54b3b51b1b9bdf292289d383312
axx4136 <70749495+axx4136@users.noreply.github.com>
2020-11-17 22:36:29 +0800

    添加初过滤功能

--- 歌曲推荐/Feature.py
+++ 歌曲推荐/Feature.py
@@ -8,6 +8,7 @@ from functools import reduce
 import csv
 import pygame
 import time
+
 filename = os.listdir("./Chorus/")
 # data_set = []
 # for file in filename:
@@ -31,21 +32,30 @@ filename = os.listdir("./Chorus/")
 # print(X)
 
 
-song_name=input("输入歌曲名：")
-name_id_dic={}
-name_feature_dic={}
-csv_reader=csv.reader(open('./csv/csvData.csv',encoding='utf-8'))
+song_name = input("输入歌曲名：")
+name_id_dic = {}
+name_bpm_dic = {}
+name_feature_dic = {}
+csv_reader = csv.reader(open('./csv/csvData.csv', encoding='utf-8'))
 for row in csv_reader:
-    name_id_dic[row[1]]=row[0]
-    if row[3]!="song_feature":
+    name_bpm_dic[row[1]] = row[2]
+    name_id_dic[row[1]] = row[0]
+    if row[3] != "song_feature":
         featureList = row[3]
         featureList = eval(featureList)
-        name_feature_dic[row[1]]=featureList
+        name_feature_dic[row[1]] = featureList
+# song_bpm = name_bpm_dic[song_name]
+# target_name_feature_dic = {}
+# target_name_id_dic = {}
+# for key in name_feature_dic.keys():
+#     if abs(float(name_bpm_dic[key]) - float(song_bpm)) < 10:
+#         target_name_feature_dic[key] = name_feature_dic[key]
+#         target_name_id_dic[key] = name_id_dic[key]
 result = []
-z=int(name_id_dic[song_name])-1
+z = int(name_id_dic[song_name]) - 1
 for j in filename:
     sum_ = 0
-    j=j.split("_")[0]
+    j = j.split("_")[0]
     if (song_name == j):
         result.append(-99)
         continue
@@ -56,12 +66,12 @@ for j in filename:
         B = sqrt(reduce(lambda x, y: x + y, map(lambda x: x * x, name_feature_dic[j])))
         answer = sum_ / (A * B)
         result.append(answer)
-print(song_name + ":" + filename[result.index(max(result))].split(".")[0] + ":%f" % (max(result)))
+print(song_name + ":" + filename[result.index(max(result))].split(".")[0].split("_")[0] + ":%f" % (max(result)))
 print(filename[result.index(max(result))].split(".")[0].split("_")[0])
 
-
 pygame.mixer.init()
-pygame.mixer.music.load("./SongWav/"+filename[result.index(max(result))].split(".")[0].split("_")[0]+".wav")
+print("Now Playing " + filename[result.index(max(result))].split(".")[0].split("_")[0])
+pygame.mixer.music.load("./SongWav/" + filename[result.index(max(result))].split(".")[0].split("_")[0] + ".wav")
 pygame.mixer.music.play(start=0.0)
 time.sleep(300)
 pygame.mixer.music.stop()
