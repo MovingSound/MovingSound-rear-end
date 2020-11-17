2474977ff37a149dcb2a0ca11be4db71376885ab
axx4136 <70749495+axx4136@users.noreply.github.com>
2020-11-17 22:38:43 +0800

    修改获取高潮片段不固定秒数

--- 获取歌曲高潮片段/selectchorus.py
+++ 获取歌曲高潮片段/selectchorus.py
@@ -4,5 +4,12 @@ from pychorus import find_and_output_chorus
 class mxc:
     def mxccccccc(self):
         filelist=os.listdir("./SongWav/")
+        self.time=[30,15,10,5]
         for file in filelist:
-            chorus_start_sec = find_and_output_chorus("./SongWav/"+file,"./Chorus/"+file.split(".")[0]+"_high.wav",15)
+            self.count = 0
+            chorus_start_sec= find_and_output_chorus("./SongWav/"+file,"./Chorus/"+file.split(".")[0]+"_high.wav", 60)
+            while chorus_start_sec ==None:
+                chorus_start_sec = find_and_output_chorus("./SongWav/"+file,"./Chorus/"+file.split(".")[0]+"_high.wav", self.time[self.count])
+                self.count += 1
+                if self.count > 3:
+                    break
