c9c9f43e92791a3e0023f6d1e6c0a69fbbfd44cc
101347 <70749481+101347@users.noreply.github.com>
2020-11-24 10:36:36 +0800

    在原有基础加入了插入用户反馈功能以及半完成的插入歌曲信息功能

--- 音乐曲库信息数据库/create database.py
+++ 音乐曲库信息数据库/create database.py
@@ -1,21 +1,17 @@
 import pymysql
+
 class create():
     def create_database(self):
         try:
-            self.con = pymysql.connect(host="127.0.0.1", port=3306, user="root", passwd="123456", db="mydb",
-                                       charset="utf8")
-            self.cursor = self.con.cursor(pymysql.cursors.DictCursor)
-            self.cursor.execute("drop database if exists MovingSound")
-            self.cursor.execute("create database MovingSound")
-            self.cursor.execute("use MovingSound")
+            self.cursor.execute("drop database if exists MovingSound;")
+            self.cursor.execute("create database MovingSound;")
+            self.cursor.execute("use MovingSound;")
         except Exception as err:
             print(err)
 
-
     def create_msg(self):
         try:
-            self.cursor = self.con.cursor(pymysql.cursors.DictCursor)
-            self.cursor.execute("DROP TABLE IF EXISTS msg")
+            self.cursor.execute("DROP TABLE IF EXISTS msg;")
             self.cursor.execute("CREATE TABLE IF NOT EXISTS msg(mid varchar(32) PRIMARY KEY,"   # 歌曲号
                             "mname varchar(20) NOT NULL,"                                       # 歌曲名
                             "mfeature varchar(20) NOT NULL,"                                    # 歌曲特征向量
@@ -24,15 +20,55 @@ class create():
                             "mlanguage varchar(20),"                                            # 语种
                             "mtime varchar(20) NOT NULL,"                                       # 歌曲时间
                             "mpicture longblob,"                                                # 歌曲封面
-                            "mdate varchar(20) NOT NULL")                                       # 歌曲发行日期
+                            "mdate varchar(20) NOT NULL);")                                       # 歌曲发行日期
         except Exception as err:
             print(err)
-
 
     def create_feedback(self):
         try:
-            self.cursor.execute("DROP TABLE IF EXISTS feedback")
-            self.cursor.execute("CREATE TABLE IF NOT EXISTS feedback(uid varchar(32) PRIMARY KEY")    # 用户号
+            self.cursor.execute("DROP TABLE IF EXISTS feedback;")
+            self.cursor.execute("CREATE TABLE IF NOT EXISTS feedback(uid varchar(32) PRIMARY KEY);")    # 用户号
             # 后续获取用户对某首歌的反馈就增加一列，值为1为喜欢，值为0则为不喜欢
         except Exception as err:
             print(err)
+
+    def insert_feedback_like(self,uid,song_name):
+        try:
+            self.cursor.execute("alter table feedback add column" + song_name + " varchar(2) not null")
+            sql = "insert into feedback (uid,column"+song_name+") value ("+str(uid)+","+str(1)+")"
+            self.cursor.execute(sql)
+        except Exception as err:
+            print(err)
+
+    def insert_feedback_dislike(self,uid,song_name):
+        try:
+            self.cursor.execute("alter table feedback add column" + song_name + " varchar(2) not null")
+            sql = "insert into feedback (uid,column"+song_name+") value ("+str(uid)+","+str(0)+")"
+            self.cursor.execute(sql)
+        except Exception as err:
+            print(err)
+
+    def insert_msg(self,mid,mname,mfeature,mtype,mpath,mlanguage,mtime,mpicture,mdate):
+        try:
+            self.cursor.execute(
+                "insert into mooc(mid,mname,mfeature,mtype,mpath,mlanguage,mtime,mpicture,mdate) values(%s,%s,%s,%s,%s,%s,%s,%s,%s)",
+                (mid,mname,mfeature,mtype,mpath,mlanguage,mtime,mpicture,mdate))
+        except Exception as err:
+            print(err)
+
+    def test(self):
+        self.con = pymysql.connect(host="127.0.0.1", port=3306, user="root", passwd="123456", db="mydb",
+                                   charset="utf8")
+        self.cursor = self.con.cursor(pymysql.cursors.DictCursor)
+        #self.create_database()
+        #self.create_msg()
+        #self.create_feedback()
+        #self.insert_feedback_like(123,name)
+        #self.con.commit()
+        #self.insert_msg
+        print("complete")
+
+name = "123"
+create().test()
+
+
