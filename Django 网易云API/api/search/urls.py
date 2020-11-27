379b3afd31a21494be8edd10f1e546af71d80587
axx4136 <70749495+axx4136@users.noreply.github.com>
2020-11-27 19:39:53 +0800

    Add files via upload

--- Django 网易云API/api/search/urls.py
+++ Django 网易云API/api/search/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
+    path('default', views.default),
+    path('suggest', views.suggest),
+    path('multimatch', views.multimatch),
+    path('hot', views.hot),
+    path('hotdetail', views.hotdetail),
 ]
--- Django 网易云API/api/song/urls.py
+++ Django 网易云API/api/song/urls.py
@@ -4,4 +4,15 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('url', views.url),
+    path('lyric', views.lyric),
+    path('check', views.check),
+    path('detail', views.detail),
+    path('recommend', views.recommend),
+    path('newsong', views.newsong),
+    path('simi', views.simi),
+    path('top', views.top),
+    path('simiuser', views.simiuser),
+    path('new', views.new),
+    path('like', views.like),
+    path('intelligence', views.intelligence),
 ]
